51d3d1c66bf26054aff07dd39a76724fd591fefc
tranphuquy19 <tranphuquy19@gmail.com>
2018-10-17 22:19:40 +0700

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 16057);
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect(iPEndPoint);
+            Console.WriteLine("Conected to Server!");
+
+            StreamReader streamReader = new StreamReader(tcpClient.GetStream());
+            StreamWriter streamWriter = new StreamWriter(tcpClient.GetStream());
+            while(true)
+            {
+                Console.WriteLine("Client: ");
+                streamWriter.WriteLine(Console.ReadLine());//Allways use .WriteLine() to send data to streamReader in Server
+                streamWriter.Flush();
+                string recevice = streamReader.ReadLine();
+                Console.WriteLine("Server: " + recevice);
+            }
+            streamReader.Close();
+            streamWriter.Close();
+            tcpClient.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any,16057);
+            TcpListener tcpListener = new TcpListener(ip);
+
+            tcpListener.Start();
+            Console.WriteLine("Litening...");
+
+            TcpClient tcpClient = tcpListener.AcceptTcpClient();
+            Console.WriteLine("Conected to Client!");
+
+            StreamReader streamReader = new StreamReader(tcpClient.GetStream());
+            StreamWriter streamWriter = new StreamWriter(tcpClient.GetStream());
+
+            while(true)
+            {
+                string receive = streamReader.ReadLine();
+                Console.WriteLine("Client: " + receive);
+
+                Console.WriteLine("Server: ");
+                streamWriter.WriteLine(Console.ReadLine());
+                streamWriter.Flush();
+            }
+            streamReader.Close();
+            streamWriter.Close();
+            tcpClient.Close();
+        }
+    }
+}
